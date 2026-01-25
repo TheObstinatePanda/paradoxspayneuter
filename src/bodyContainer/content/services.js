5d6d9aefeae1fd6e7bb57d93018e3517e455de0c
OPWebDev <j.m.newman.101@gmail.com>
2026-01-25 14:20:17 -0500

    updating services

--- src/bodyContainer/content/services.js
+++ src/bodyContainer/content/services.js
@@ -29,9 +29,6 @@ function services() {
                             <ul>
                                 <li>Add $44 for dogs 50-69 lbs</li>
                                 <li>Add $88 for dogs 70-89 lbs</li>
-                                <li>Add $139 for dogs 90-109 lbs</li>
-                                <li>Add $176 for dogs 110-129 lbs</li>
-                                <li>Add $216 for dogs 130-150 lbs</li>
                                 <li>Add $165 for dogs that are pregnant or have an abnormal uterus.</li>
                             </ul>
                         </li>
@@ -40,28 +37,30 @@ function services() {
                     </ul>
                 </div>
                 <div id='cat'>
-                    <h2>Cats</h2>
+                    <h2>Cats and Kittens</h2>
                     <ul className='prices'>
-                        <li>Spay(female) $70</li>
+                        <li>Spay(female) $70 (Add $20-$65 for cats that are pregnat or in heat, the cose depends on how far along the pregnancy is.)</li>
                         <li>Neuter(male) $47</li>
-                        <li>
-                            <ul>
-                                <li>Add $20-$65 for cats that are pregnat or in heat, the cose depends on how far along the pregnancy is.</li>
-                            </ul>
-                        </li>
-                        <p>All cats must be in a carrer or trap or we will not accept them.</p>
+                        
+                        <p>All cats must be in a carrier or trap or we will not accept them. An injection for additional pain management is available for $10. Additional take home pain meds or antibiotics are $18 each.</p>
+                    </ul>
+                    <h2>Feral Cats</h2>
+                    <ul>
+                        <p>No appointment necessary. Drop off Tues-Thurs 8:30 AM to 10 AM. Max of 3 cats per day. All ferals must be in a trap and Ear Tip is required!</p>
+                        <li>Feral Cat Spay(female)$77 (Add$22-$85 for cast that are pregnant. Pricing depends on how far along the pregnancy is.)</li>
+                        <li>Feral Cat Neuter(male)$52</li>
                     </ul>
                     <h2>Other Services</h2>
                     <ul className='prices'>
                         <li>Dog Rabies Vaccine $20</li>
                         <li>Dog DHPP/DHLPP Vaccine $17</li>
                         <li>Dog Kennel Cough/Bordetella Vaccine $17</li>
-                        <li>Dog Heartworm Test $30</li>
+                        <li>Dog Heartworm Test $33</li>
                         <li>Cat Rabies Vaccine (Purevax) $27</li>
-                        <li>Feral Cat Rabies Vaccine $10</li>
+                        <li>Feral Cat Rabies Vaccine $27</li>
                         <li>Cat FRCP Vaccine $17</li>
-                        <li>Cat FeLV/FIV Combo Test $40</li>
-                        <li>Cat Cat FeLV/Feline Leukemia Vaccine $31</li>
+                        <li>Cat FeLV/Feline Leukemia Vaccine $31</li>
+                        <li>Cat FeLV/FIV Combo Test $44</li>
                         <li>Microchip $28</li>
                         <li>Fecal Test $17</li>
                         <li>Flea/Tick Treament - Cost varies</li>
